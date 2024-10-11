35082876c9367216d945eeaa30c017b0ca3394d6
Coaris <Coaris@outlook.com>
2024-10-11 12:46:28 +0100

    Main Menu Frame

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,19 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour {
         private static GameManager instance;
         public static GameManager Instance { get { return instance; } }
+        
 
         private void Awake() {
                 if (instance == null) instance = this;
                 else Destroy(gameObject);
                 DontDestroyOnLoad(instance);
-        }
-        
-        private void LoadScene(string sceneName) {
-                SceneManager.LoadScene(sceneName);
         }
 }
--- Assets/Scripts/OS/SceneConfig.cs
+++ Assets/Scripts/OS/SceneConfig.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
-[CreateAssetMenu(fileName ="SceneConfig",menuName ="Configurations/SceneConfig")]
-
+[CreateAssetMenu(fileName = "SceneConfig", menuName = "Configurations/SceneConfig")]
 public class SceneConfig : ScriptableObject {
-        public string sceneName;
-        public SceneAsset scene;
+        public SceneAsset MainMenu;
+        public SceneAsset Settings;
+        public SceneAsset Credits;
+        public SceneAsset NewGame;
 }
